04864bfdfa856be26834a42f45cbe23343cba221
00009569 <wiut00009569@gmail.com>
2021-04-06 14:00:12 +0500

    Recent changes,some pug files are added

---
+++ README.md
@@ -0,0 +1,29 @@
+# Blogs web application
+
+### This web application give opportunity to users to create, delete and update functionalities with blogs
+
+To run this application locally you have to follow below steps:
+
+1. Clone this repository
+```bash
+
+git clone repisotory link
+```
+
+2. Install dependicies
+```bash
+cd[project foolder]
+npm install
+```
+
+3. Run the blog application
+```bash
+node app.js
+``` 
+
+### Web application
+- express.js (https://expressjs.com/)
+- pug.js (https://pugjs.org/api/getting-started.html)
+
+### Web application repisitory on github
+[Link to github]
--- app.js
+++ app.js
@@ -1,1 +1,84 @@
+const express = require('express')
+const app = express()
 
+const fs = require('fs')
+
+app.set('view engine', 'pug')
+
+app.use('/static', express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+
+//localhost is 8000
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/create', (req, res) => {
+    res.render('create')
+})
+
+app.post('/create', (req, res) => {
+    const title = req.body.title
+    const description = req.body.description
+
+    if (title.trim() === '' && description.trim() === '') {
+        res.render('create', { error: true })
+    } else {
+        fs.readFile('./data/blogs.json', (err, data) => {
+            if (err) throw err
+
+            const blogs = JSON.parse(data)
+
+            blogs.push({
+                id: id(),
+                title: title,
+                description: description,
+            })
+
+            fs.writeFile('./data/blogs.json', JSON.stringify(blogs), err => {
+                if (err) throw err
+
+                res.render('create', { success: true })
+            })
+        })
+
+    }
+})
+
+
+
+app.get('/blogs', (req, res) => {
+
+    fs.readFile('./data/blogs.json', (err, data) => {
+        if (err) throw err
+
+        const blogs = JSON.parse(data)
+
+        res.render('blogs', { blogs: blogs })
+    })
+})
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id
+    fs.readFile('./data/blogs.json', (err, data) => {
+        if (err) throw err
+
+        const blogs = JSON.parse(data)
+
+        const blog = blogs.filter(blog => blog.id == id)[0]
+
+        res.render('detail', { blog: blog })
+    })
+
+
+})
+
+app.listen(8000, err => {
+    if (err) console.log(err)
+
+    console.log('Server is running on port 8000...')
+})
+
+function id() {
+    return '_' + Math.random().toString(36).substr(2, 9);
+}
